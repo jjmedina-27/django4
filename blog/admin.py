a9686b94a036ce4c2c9a0e3252b9a7cce64d7b94
J.J. Medina <133665715+jjmedina-27@users.noreply.github.com>
2024-09-28 10:26:43 -0400

    list_display and list_filter

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Post
 
-# Register your models here.(
-admin.site.register(Post)
+# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "body",
+    )
+    list_filter = (
+        "title",
+        "author",
+    )
+
+admin.site.register(Post,PostAdmin)
